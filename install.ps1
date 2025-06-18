a8fde5b3b190b528aaf570f7ded7035f800fc3f6
spc-kororo <t.katouno@supreedom.com>
2025-06-18 13:26:29 +0900

    フォントインストール処理がエラーで動いていなかったため見直し
    参考：https://qiita.com/tkmry/items/c6a7a008f1d839481686

--- install.ps1
+++ install.ps1
@@ -1,3 +1,29 @@
+function downloadFile {
+    [OutputType([void])]
+    param([string]$url, [string]$saveDir)
+    $fileName = Split-Path $url -Leaf
+    $downloadFilePath = Join-Path -Path $saveDir -ChildPath $fileName
+    Invoke-WebRequest $url -OutFile $downloadFilePath
+}
+
+function loadInstalledFontNames {
+    [OutputType([string])]
+    param()
+
+    Add-Type -AssemblyName System.Drawing
+    $userFontsDir = "$env:LOCALAPPDATA\Microsoft\Windows\Fonts"
+
+    $installedSystemFonts = (New-Object System.Drawing.Text.InstalledFontCollection)
+    $installedUserFonts = New-Object System.Drawing.Text.PrivateFontCollection
+    Get-ChildItem $userFontsDir | ForEach-Object {
+        $installedUserFonts.AddFontFile($_.FullName)
+    }
+
+    return $installedSystemFonts.Families.Name + $installedUserFonts.Families.Name
+}
+
+### メイン処理 ###
+
 # 管理者権限へ昇格
 # 参考：https://qiita.com/sakekasunuts/items/63a4023887348722b416#ps1実行時に自動的に昇格させたい
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) {
@@ -14,7 +40,7 @@ $REPO_HOME = $PSScriptRoot
 # 作業ディレクトリの作成
 $tempDir = $env:TEMP | Join-Path -ChildPath "dotfiles"
 if (Test-Path $tempDir) {
-    Remove-Item -Path $tempDir\* -Force
+    Remove-Item -Path $tempDir\* -Force -Recurse
 }
 else {
     mkdir $tempDir
@@ -22,18 +48,30 @@ else {
 
 # Font
 ## Nerd Fontのダウンロード
-downloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip", $tempDir)
+downloadFile "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Inconsolata.zip" $tempDir
 
 ## プログラミングフォントのダウンロード
-downloadFile("https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip", $tempDir)
+downloadFile "https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip" $tempDir
 
-## インストール
-$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+## プログラミングフォントインストール前の準備
+$systemFontsDir = "$env:windir\Fonts"
+$fonts = (New-Object -ComObject Shell.Application).Namespace($systemFontsDir)
+$installedFontNames = loadInstalledFontNames
+
+## プログラミングフォントのインストール
 Get-ChildItem -Path $tempDir\* -Include *.zip | ForEach-Object {
     $destDir = $_.FullName.Substring(0, $_.FullName.LastIndexOf('.'))
     Expand-Archive -Path $_.FullName -DestinationPath $destDir
     Get-ChildItem -Path $destDir\* -Include *.tt? | ForEach-Object {
-        $fonts.CopyHere($_.fullname)
+        # フォントファイルからフォント名を取得する
+        $pfc = New-Object System.Drawing.Text.PrivateFontCollection
+        $pfc.AddFontFile($_.FullName)
+        $fontName = $pfc.Families[0].Name
+
+        # 未インストールの場合のみ実行
+        if (!($installedFontNames | Where-Object { $_ -eq $fontName })) {
+            $fonts.CopyHere($_.fullname)
+        }
     }
 }
 
@@ -80,10 +118,3 @@ Get-ChildItem -Path $REPO_HOME/config/* | ForEach-Object {
 }
 
 Pause
-
-##########################################################
-function downloadFile([string]$url, [string]$saveDir) {
-    $fileName = Split-Path $url -Leaf
-    $downloadFilePath = $saveDir | Join-Path -ChildPath $fileName
-    Invoke-WebRequest $url -OutFile $downloadFilePath
-}
